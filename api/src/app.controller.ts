4726d2838c04ee5b8fe790ed239106ddf65e2e7a
renansilva15 <renangabrielsilva150@gmail.com>
2024-11-30 12:25:07 -0300

    feat: add swagger/openapi specification

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -1,8 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOkResponse } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
   @Get()
+  @ApiOkResponse({
+    description: 'Returns an object representing the API status',
+    example: { status: 'Ok' },
+  })
   healthCheck(): { status: 'Ok' } {
     return { status: 'Ok' };
   }
--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Country Info')
+    .setVersion('v1')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, documentFactory);
+
+  app.setGlobalPrefix('api');
+
   await app.listen(process.env.PORT ?? 3333);
 }
 bootstrap();
